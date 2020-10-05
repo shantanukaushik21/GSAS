f5da73a75367f28c429a981b7c3112e2e564bb20
unknown <shantanu.kaushik21@gmail.com>
2020-10-05 20:55:26 +0530

    schemedao

--- src/com/gsas/dao/SchemeDao.java
+++ src/com/gsas/dao/SchemeDao.java
@@ -4,4 +4,7 @@ import com.gsas.model.SchemeVO;
 
 public interface SchemeDao {
 	public void storeScheme(SchemeVO scheme);
+	public void editScheme(SchemeVO scheme);
+	public SchemeVO getSchemeDetails(Long schemeId);
+	public SchemeVO[] getAllScheme();
 }
